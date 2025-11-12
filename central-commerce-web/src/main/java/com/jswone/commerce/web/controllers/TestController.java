31281636921f07c05f7502e4ffc72e6ade3d40df
anuragwadhwa <v_anurag.wadhwa@jsw.in>
2025-11-12 20:36:24 +0530

    JSWOC-18983 - Added server port for application.

---
+++ central-commerce-web/src/main/java/com/jswone/commerce/web/controllers/TestController.java
@@ -0,0 +1,19 @@
+package com.jswone.commerce.web.controllers;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("test")
+public class TestController {
+
+    @GetMapping("/ping")
+    public String getAuthToken() {
+        return "ping successful";
+    }
+}
