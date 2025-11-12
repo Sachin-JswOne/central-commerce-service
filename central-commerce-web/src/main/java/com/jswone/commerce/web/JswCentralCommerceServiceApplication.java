fc48447b58d26364fdde9331ea732b187a58f7ea
anuragwadhwa <v_anurag.wadhwa@jsw.in>
2025-11-12 19:51:13 +0530

    JSWOC-18983 - Added initial project setup for Central Commerce Service.

--- central-commerce-web/src/main/java/com/jswone/commerce/web/JswCentralCommerceServiceApplication.java
+++ central-commerce-web/src/main/java/com/jswone/commerce/web/JswCentralCommerceServiceApplication.java
@@ -1,13 +1,13 @@
-package com.jswone;
+package com.jswone.commerce.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class JswCentralCommerceServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(JswCentralCommerceServiceApplication.class, args);
 	}
 
 }
