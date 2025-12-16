623343bdc28b6db2027d2f559efea05425880351
Ajeet Yadav <v_ajeet.yadav@jsw.in>
2025-12-16 07:52:18 +0530

    code rearrange

--- central-commerce-core/src/main/java/com/jswone/commerce/core/rest/CentralCatalogueClient.java
+++ central-commerce-core/src/main/java/com/jswone/commerce/core/rest/CentralCatalogueClient.java
@@ -1,5 +1,8 @@
 package com.jswone.commerce.core.rest;
 
+import com.jswone.commerce.core.model.CatalogueBreadCrumbData;
+import com.jswone.commerce.core.model.CatalogueCategoryTree;
+import com.jswone.commerce.core.model.ImageMetadata;
 import com.jswone.commerce.core.model.request.ProductBulkRequest;
 import com.jswone.commerce.core.model.request.ProductTypeBulkRequest;
 import com.jswone.commerce.core.model.request.Search.SearchRequest;
@@ -14,9 +17,7 @@ import java.util.List;
 
 public interface CentralCatalogueClient {
     ProductSearchResponse genericSearch(SearchRequest searchRequest);
-
     ProductSearchResponse genericSearchFacetsOnly(SearchRequest searchRequest);
-
     ProductBulkResponse bulkMMIDResponse(ProductBulkRequest productBulkRequest);
     ProductTypeBulkResponse bulkTypeIdResponse(ProductTypeBulkRequest productTypeBulkRequest);
     Map<String, ImageMetadata> fetchImagesForMmIds(Set<String> productMmIds);
