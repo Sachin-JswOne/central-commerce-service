25e22a6f43496deffe629931ed5734fca8b9aa42
Shubham Bansal <shubham.bansal@jsw.in>
2025-12-18 22:49:16 +0530

    reverted

--- central-commerce-core/src/main/java/com/jswone/commerce/core/repository/elastic/impl/RecentSearchElasticIndexRepositoryImpl.java
+++ central-commerce-core/src/main/java/com/jswone/commerce/core/repository/elastic/impl/RecentSearchElasticIndexRepositoryImpl.java
@@ -1,8 +1,12 @@
 package com.jswone.commerce.core.repository.elastic.impl;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.SortOrder;
+import co.elastic.clients.elasticsearch._types.mapping.FieldType;
 import co.elastic.clients.elasticsearch.core.IndexRequest;
 import co.elastic.clients.elasticsearch.core.IndexResponse;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
 import com.jswone.commerce.core.constants.ElasticConstants;
 import com.jswone.commerce.core.model.elastic.index.RecentSearchIndex;
 import com.jswone.commerce.core.repository.elastic.RecentSearchElasticIndexRepository;
@@ -11,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -34,27 +39,5 @@ public class RecentSearchElasticIndexRepositoryImpl implements RecentSearchElast
             log.error("Failed to index data : {}", recentSearch.getSearchId());
         }
         log.info("Indexed Search logs ID: {}", response.id());
-    }
-
-    @Override
-    public SearchResponse<RecentSearchIndex> getRecentSearches(String userId) throws IOException {
-        SearchRequest request = SearchRequest.of(s -> s
-                .index(List.of(ElasticConstants.RECENT_SEARCH_INDEX))
-                .sort(so -> so
-                        .field(f -> f
-                                .field("timestamp")
-                                .order(SortOrder.Desc)
-                                .unmappedType(FieldType.Float)
-                        )
-                )
-                .query(recentSearchQueryBuilder.getRecentSearchQuery(userId))
-                .size(1000)
-        );
-
-        log.info("Executing search with request: {}", request);
-        return elasticsearchClient.search(
-                request,
-                RecentSearchIndex.class
-        );
     }
 }
