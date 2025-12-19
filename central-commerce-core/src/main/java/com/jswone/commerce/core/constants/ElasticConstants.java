e2e526be9063645e3421f2f5369b8452de4973dd
anuragwadhwa <v_anurag.wadhwa@jsw.in>
2025-12-19 15:01:33 +0530

    JSWOC-19346-Added implementation for clear recent searches.

--- central-commerce-core/src/main/java/com/jswone/commerce/core/constants/ElasticConstants.java
+++ central-commerce-core/src/main/java/com/jswone/commerce/core/constants/ElasticConstants.java
@@ -2,7 +2,9 @@ package com.jswone.commerce.core.constants;
 
 public class ElasticConstants {
 
-    public static final String RECENT_SEARCH_INDEX = "recent_searches";
     private ElasticConstants() {
     }
+
+    public static final String RECENT_SEARCH_INDEX = "recent_searches";
+
 }
